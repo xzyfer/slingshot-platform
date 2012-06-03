b169ce7d044b498d384b2c6f49bbd0224344676f
xzyfer <xzyfer@gmail.com>
2012-06-03 10:51:19 +1000

    Wire up search button

--- index.rb
+++ index.rb
@@ -32,6 +32,11 @@ get '/search/:name' do
   erb :search_results
 end
 
+post '/search' do
+  @users = mongo('user').find('name' => /.*#{params[:name]}.*/i).to_a
+  erb :search_results
+end
+
 get '/create/:name/:times' do
   images = ["1-1", "1-2", "2-1", "2-2", "2-3", "2-4", "2-5", "3", "3-1", "3-2", "3-3", "3-4", "3-5", "3-6", "4", "4-1", "4-2", "4-3", "4-4", "4-5", "4-6", "4-7", "4-8", "5", "5-1", "5-2", "5-3", "5-4", "5-5", "5-6", "6", "6-1", "6-2", "6-3", "6-4", "6-5", "6-6", "6-7", "6-8"]
   (1..params[:times].to_i).to_a.each { |i|
